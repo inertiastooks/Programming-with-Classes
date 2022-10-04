86c3ef89be0ecbbe1ec94705a8dfdf16b6c940cf
ltstucki <inertiastooks@gmail.com>
2022-10-04 14:36:35 -0600

    card and player

--- Hi-Lo-Game/card_Game.cs
+++ Hi-Lo-Game/card_Game.cs
@@ -2,9 +2,11 @@
 static void Main(string[] args)
 {
     Card card = new Card();
+    Player player = new Player();
 
-    card.draw_card();
+    Console.WriteLine($"The card that was drawn is {card.draw_card()}");
 
+    Console.WriteLine($"Your guess is {player.players_guess()}");
 
 }
 
--- Hi-Lo-Game/player.cs
+++ Hi-Lo-Game/player.cs
@@ -1,9 +1,13 @@
+
 public class Player
 {
-    public string players_guess(){
+    public int players_guess(){
 
         Console.WriteLine("Please guess a number between 1-13: ");
-        string? player_guess = Console.ReadLine();
+        string? player_guess_string = Console.ReadLine();
+
+        int player_guess = Convert.ToInt32(player_guess_string);
+
         return player_guess;
 
     }
