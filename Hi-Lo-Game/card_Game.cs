870de9f91ff4157cd580ff17d214b1dd7d09933c
inertiastooks <ltstucki@byui.edu>
2022-10-06 21:55:00 -0600

    Finished High-Low

--- Hi-Lo-Game/card_Game.cs
+++ Hi-Lo-Game/card_Game.cs
@@ -3,23 +3,69 @@ static void Main(string[] args)
 {
     Player player = new Player();
     Card card = new Card();
-    int card_1 = card.draw_card();
-    int card_2 = card.draw_card();
+    Validator validator = new Validator();
+
     Console.WriteLine("============== Instructions ==============");
     Console.WriteLine($"This is the game of High Low.\nA card will be drawn and you will guess whether the next card is higher or lower");
     Console.WriteLine("If you guess right, you get 100 points, if you guess wrong, you will lose 75.");
-    Console.WriteLine("You start with 300. Good Luck!");
-    Console.WriteLine($"The card that was drawn is {card_1}");
-    Console.WriteLine($"Your guess is none");
-    Console.WriteLine($"The new that was drawn is {card_2}");
-
-
+    Console.WriteLine("You start with 300. Good Luck!\n");
     
-    //player.score = player.score - 25;
-    Console.WriteLine(player.score);
+    bool is_playing = true;
+
+    while(is_playing){
+        int card1 = card.draw_card(); //Draws the first card
+        int card2 = card.draw_card(); //Draws the second Card
+
+        Console.WriteLine($"The card that was drawn is {card1}"); //Shows user what the first card is
+
+        string? high_or_low = validator.validate_players_guess(); //Checks whether the user inputed a valid Response
+
+        Console.WriteLine($"The next card that was drawn is {card2}"); //
+
+        if(high_or_low == "h" || high_or_low == "H"){
+            if (card1 < card2){
+                player.score = player.score + 100;
+                Console.WriteLine($"+100 Pts! Your score is {player.score}");
+            }
+            else if (card1 > card2){
+                player.score = player.score - 75;
+                Console.WriteLine($"-75 Pts! Your score is {player.score}");
+            }
+            else{
+                Console.WriteLine($"It was a tie! Your score is still {player.score}");
+            }
+        }
+
+        else if(high_or_low == "l" || high_or_low == "L"){
+            if (card1 > card2){
+                player.score = player.score + 100;
+                Console.WriteLine($"+100 Pts! Your score is {player.score}");
+            }
+            else if (card1 < card2){
+                player.score = player.score - 75;
+                Console.WriteLine($"-75Pts! Your score is {player.score}");
+            }
+            else{
+                player.score = player.score -75;
+                Console.WriteLine($"The cards were the same! Which means you were still wrong! -75 Pts! Your score is {player.score}");
+            }
+        }
+        
+        string? play_again = validator.validate_play_again();
+        if (play_again == "Y" || play_again == "y")
+        {
+            Console.WriteLine("\n\n---New Game---");
+        }
+        else
+        {
+            Console.WriteLine("Thanks for playing!");
+            is_playing = false;
+        }
+
+    }
+
 }
-
-
+}
 
 
 
@@ -46,7 +92,3 @@ static void Main(string[] args)
 
         else return score;
 */
-
-
-
-}
--- Hi-Lo-Game/validator.cs
+++ Hi-Lo-Game/validator.cs
@@ -1,32 +1,52 @@
 public class Validator
 {
     public string? validate_players_guess()
+    //This function will check whether the user inputs a valid response, only accepting upper/lowercase L and H.
+    //It also will prompt them to try again if it doesn't recieve the input wanted.
     {
+
+        Console.Write("Higher or Lower?: ");
+        string? player_guess = Console.ReadLine();
+
         bool player_correct_guess = false;
-            Console.Write("Higher or Lower?: ");
-            string? player_guess = Console.ReadLine();
-            if(player_guess != "H" || player_guess != "h" || player_guess != "L" ||player_guess != "l")
-            {
-                Console.WriteLine("Error! : Please guess 'H' or 'L'!");
-                
-            }
-            else
+        while(player_correct_guess == false){
+            if(player_guess == "H" || player_guess == "h" || player_guess == "L" || player_guess == "l")
             {
                 player_correct_guess = true;
             }
-            
-            return player_guess;
+            else
+            {
+                Console.WriteLine("Error! : Please guess 'H' or 'L'!");
+                Console.Write("Higher or Lower?: ");
+                player_guess = Console.ReadLine();
+            }
         }
+            
+        return player_guess;
     }
 
+    public string? validate_play_again()
+    //This function will check whether the user inputs a vaid response to playing again, only accepting upper/lowercase Y/N.
+    //It also will prompt them to try again if it  doesn't recieve the input wanted.
+    {
+        Console.Write("Would you like to play again? Y/N: ");
+        string? play_again = Console.ReadLine();
+
+        bool player_correct_response = false;
+        while(player_correct_response == false)
+        {
+            if (play_again == "Y" || play_again == "y" || play_again == "N" || play_again == "n")
+            {
+                player_correct_response = true;
+            }
+            else
+            {
+                Console.WriteLine("Error : Please Choose 'Y' or 'N' to play again or not!");
+                Console.Write("Would you like to play again? Y/N: ");
+                play_again = Console.ReadLine();
+            }
+        }
+        return play_again;
+    }
 
 }
-
-/*    public string? players_guess(){
-
-        Console.Write("Higher or lower?: ");
-        string? player_guess = Console.ReadLine();
-        
-        return player_guess;
-
-    }*/
